42edd4c872117ea8c820c46043d08d1b91d155f3
ZexiGong <gong.ze@husky.neu.edu>
2020-04-13 23:39:21 +0800

    web.xml

--- src/main/java/com/zx/controller/EmailController.java
+++ src/main/java/com/zx/controller/EmailController.java
@@ -3,13 +3,14 @@ package com.zx.controller;
 import com.zx.domain.Email;
 import com.zx.service.IEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/email")
 public class EmailController {
     @Autowired
